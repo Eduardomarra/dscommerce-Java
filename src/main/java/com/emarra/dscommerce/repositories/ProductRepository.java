352a899ed834dd82f204f7f4f4aa0661c94a704b
eduardomarra <eduardomarra@gmail.com>
2024-04-16 16:47:52 -0300

    Added ProductRepository

--- src/main/java/com/emarra/dscommerce/repositories/ProductRepository.java
+++ src/main/java/com/emarra/dscommerce/repositories/ProductRepository.java
@@ -1,4 +1,7 @@
 package com.emarra.dscommerce.repositories;
 
-public interface ProductRepository {
+import com.emarra.dscommerce.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
