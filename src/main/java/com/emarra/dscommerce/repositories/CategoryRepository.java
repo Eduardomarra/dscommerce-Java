3bab593bb20bb6d6e0a918f72a97a84a2fa8161b
Eduardo Marra <eduardo.gomes@mpdft.mp.br>
2024-10-15 18:29:21 -0300

    Get categories

---
+++ src/main/java/com/emarra/dscommerce/repositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.emarra.dscommerce.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.emarra.dscommerce.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
---
+++ src/main/java/com/emarra/dscommerce/services/CategoryService.java
@@ -0,0 +1,25 @@
+package com.emarra.dscommerce.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.emarra.dscommerce.dto.CategoryDTO;
+import com.emarra.dscommerce.entities.Category;
+import com.emarra.dscommerce.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> findAll() {
+        List<Category> result = repository.findAll();
+        return result.stream().map(CategoryDTO::new).toList();
+    }
+
+}
